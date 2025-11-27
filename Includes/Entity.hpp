deba6d5cef0562bccf8f4e476c90fea87f8eb48f
lain157 <lain157@seznam.cz>
2025-11-27 13:05:28 +0900

    Added Entity.cpp and Entity.hpp.

---
+++ Includes/Entity.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Andrew on 27/11/2025.
+//
+
+#ifndef BONK_GAME_ENTITY_HPP
+#define BONK_GAME_ENTITY_HPP
+
+
+class Entity final {
+public:
+    ~Entity() = default;
+    void update();
+};
+
+
+#endif //BONK_GAME_ENTITY_HPP
---
+++ Src/Entity.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Andrew on 27/11/2025.
+//
+
+#include "../Includes/Entity.hpp"
