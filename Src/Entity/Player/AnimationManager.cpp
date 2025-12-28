775ed5581ae0c69c79cdc1c84e2e9ec421101815
kopecon <kopecon4@gmail.com>
2025-12-29 00:58:46 +0900

    Reordered animation selection switch.

--- Src/Entity/Player/AnimationManager.cpp
+++ Src/Entity/Player/AnimationManager.cpp
@@ -16,51 +16,35 @@ void player::AnimationManager::selectAnimation() {
     using enum StateSet::ID;
 
     switch (player.getStateID()) {
-        case JUMPING : {
-            engine.set(JUMPING);
-            // engine.animationSet[JUMPING].fps = std::fabs(pPlayer->movement.walkingSpeed.y / pPlayer->movement.getSpeed().y) * 24.f;
-            break;
-        }
-        case WALKING : {
-            engine.set(WALKING);
-            // engine.pCurrentAnimation->fps = std::fabs(pPlayer->physics.velocity.x / pPlayer->movement.walkingSpeed.x) * static_cast<float>(engine.pCurrentAnimation->framesPerRow) * 2.f;
-            break;
-        }
-        case RUNNING : {
-            engine.set(RUNNING);
-            // engine.pCurrentAnimation->fps = std::fabs(pPlayer->physics.velocity.x / pPlayer->movement.runningSpeed.x) * static_cast<float>(engine.pCurrentAnimation->framesPerRow) * 2.f;
-            break;
-        }
-        case ATTACKING : {
-            engine.set(ATTACKING);
-            // engine.pCurrentAnimation->fps = static_cast<float>(engine.pCurrentAnimation->framesPerRow) * 3.f;
-            break;
-        }
-        case DYING : {
-            engine.set(DYING);
-            // animationManager.pCurrentAnimation->fps = static_cast<float>(animationManager.pCurrentAnimation->framesPerRow) * 3.f;
-            break;
-        }
-        case NONE:
-            break;
         case IDLE:
             engine.set(IDLE);
             break;
         case WINKING:
             engine.set(WINKING);
             break;
+        case WALKING:
+            engine.set(WALKING);
+            break;
+        case RUNNING:
+            engine.set(RUNNING);
+            break;
         case CROUCHING:
             engine.set(CROUCHING);
+            break;
+        case JUMPING:
+            engine.set(JUMPING);
             break;
         case DISAPPEARING:
             engine.set(DISAPPEARING);
             break;
-        case BRAKING:
-            engine.set(IDLE);
+        case DYING:
+            engine.set(DYING);
             break;
-        case STOPPING:
-            engine.set(IDLE);
+        case ATTACKING:
+            engine.set(ATTACKING);
             break;
+        default:
+            engine.set(IDLE);
     }
 }
 
