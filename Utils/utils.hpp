691e6f20e73b442e09bdc3faf54eca7f170708c7
kopecon <kopecon4@gmail.com>
2026-01-02 21:56:06 +0900

    Dividing now returns sf::Vector2f by default.

--- Utils/utils.hpp
+++ Utils/utils.hpp
@@ -47,10 +47,10 @@ namespace hadamard {
     requires (std::is_base_of_v<sf::Vector2i, sfVector>
         || std::is_base_of_v<sf::Vector2u, sfVector>
         || std::is_base_of_v<sf::Vector2f, sfVector> )
-    sfVector divide(const sfVector &first, const Vectors&... vectors) {
+    sf::Vector2f divide(const sfVector &first, const Vectors&... vectors) {
         sfVector result{};
-        result.x = (sfVector{first.x} / ... / (vectors.x));
-        result.y = (sfVector{first.y} / ... / (vectors.y));
+        result.x = (first.x / ... / (vectors.x));
+        result.y = (first.y / ... / (vectors.y));
         return result;
     }
 
@@ -84,7 +84,7 @@ namespace scalar {
     requires (std::is_base_of_v<sf::Vector2i, sfVector>
         || std::is_base_of_v<sf::Vector2u, sfVector>
         || std::is_base_of_v<sf::Vector2f, sfVector> )
-    sfVector divide(const sfVector &vector, const S &scalar) {
+    sf::Vector2f divide(const sfVector &vector, const S &scalar) {
         sfVector result{};
         result.x = (vector.x / scalar);
         result.y = (vector.y / scalar);
