70f5f25569d2b71c1d6a2cc2bd6b5503d79f2e3e
kopecon <kopecon4@gmail.com>
2026-01-22 01:03:33 +0900

    BUG: Composite getLocalBounds is broken.

--- Src/Entity/Player/RenderManager.cpp
+++ Src/Entity/Player/RenderManager.cpp
@@ -12,15 +12,38 @@
 
 namespace player {
     RenderManager::RenderManager(Player &player) : player(player) {
+        // PLAYER SPRITE
         auto &texture = player.game.textures.player;
         auto animationSheet = std::make_unique<AnimationSheet>(texture, sf::Vector2u(32, 32));
+        auto animationSheet1 = std::make_unique<AnimationSheet>(texture, sf::Vector2u(32, 32));
+        auto animationSheet2 = std::make_unique<AnimationSheet>(texture, sf::Vector2u(32, 32));
+        auto animationSheet3 = std::make_unique<AnimationSheet>(texture, sf::Vector2u(32, 32));
+        auto animationSheet4 = std::make_unique<AnimationSheet>(texture, sf::Vector2u(32, 32));
 
         auto playerSprite = std::make_unique<PlayerSprite>(player, std::move(animationSheet));
 
         playerSprite->getSprite().setScale(hd::divide(player.getCharacterSize(), playerSprite->getGlobalBounds().size));
-        
+        // TEST SPRITE
+        auto testSprite1 = std::make_unique<PlayerSprite>(player, std::move(animationSheet1));
+        auto testSprite2 = std::make_unique<PlayerSprite>(player, std::move(animationSheet2));
+        auto testSprite3 = std::make_unique<PlayerSprite>(player, std::move(animationSheet3));
+        auto testSprite4 = std::make_unique<PlayerSprite>(player, std::move(animationSheet4));
+        float distance = 100;
+        testSprite1->move({-distance, -distance});
+        testSprite2->move({distance, -distance});
+        testSprite3->move({-distance, distance});
+        testSprite4->move({distance, distance});
+
+        playerSprite->add(std::move(testSprite1));
+        playerSprite->add(std::move(testSprite2));
+        playerSprite->add(std::move(testSprite3));
+        playerSprite->add(std::move(testSprite4));
+        playerSprite->showOutline();
+
+        // RENDER
         player.render.changeRoot(std::move(playerSprite));
-        player.render.getRoot().setOrigin(player.render.getRoot().getCenter());
-        player.render.getRoot().showOutline(sf::Color::Blue);
+        auto &root = player.render.getRoot();
+        root.setOrigin(player.render.getRoot().getCenter());
+        // root.showOutline(sf::Color::Blue);
     }
 } // player
